bc5ca3d3fb4e5d53d75c4d34eaf726b19bbd7cfd
Anton Valkovskyi <u1remited@gmail.com>
2025-03-24 12:36:58 +0200

    changed breakpoints variables; added hamburger menu; js for burger; some button responsive changes

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,12 @@
+"use strict";
+
 import "/src/sass/style.scss";
+
+document.addEventListener("DOMContentLoaded", () => {
+    const headerNav = document.querySelector(".header");
+    const burger = document.querySelector(".burger");
+
+    burger.addEventListener("click", () => {
+        headerNav.classList.toggle("header__navigation_active");
+    })
+})
