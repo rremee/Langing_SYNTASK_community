293147087411ea47e6c869e246deefc51a0a3b4f
Anton Valkovskyi <u1remited@gmail.com>
2025-03-28 13:42:01 +0200

    fixed problem with infinite images; changed css animation + script for it

--- src/js/script.js
+++ src/js/script.js
@@ -3,22 +3,27 @@
 import "/src/sass/style.scss";
 
 document.addEventListener("DOMContentLoaded", () => {
-    const headerNav = document.querySelector(".header");
-    const burger = document.querySelector(".burger");
+	const headerNav = document.querySelector(".header");
+	const burger = document.querySelector(".burger");
 
-    burger.addEventListener("click", () => {
-        headerNav.classList.toggle("header__navigation_active");
-    })
-    
-    const profilesContainers = document.querySelectorAll(".profiles__items");
+	burger.addEventListener("click", () => {
+		headerNav.classList.toggle("header__navigation_active");
+	});
+
+	const profilesContainers = document.querySelectorAll(".profiles__items");
 
     profilesContainers.forEach((profilesContainer) => {
         const profilesChildren = [...profilesContainer.children];
-        const numberOfItems = profilesChildren.length;
+        const profilesChildrenNumber = profilesChildren.length;
 
-        for (let i = 0; i < numberOfItems; i++) {
-            profilesContainer.appendChild(profilesChildren[i].cloneNode(true));
+        for (let i = 0; i < 2; i++) {
+            profilesChildren.forEach((item) => {
+                profilesContainer.appendChild(item.cloneNode(true));
+            });
         }
-    });
-})
 
+        profilesContainer.addEventListener("animationiteration", () => {
+            profilesContainer.style.transform = "translateX(0)";
+        });
+    });
+});
